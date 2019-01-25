bf5bd011a673aafbe158f7f46923e4cbebe2e2ed
Monica Loria <U91605@ust-global.com>
2019-01-25 09:41:15 -0600

    my own code

--- data.js
+++ data.js
@@ -1,85 +1,110 @@
+
 var students = [
-    {id: "1122222", name: "John", score: 90 }, // 0 {Object} => students[0] => students[0].score => total  
-    {id: "2223333", name: "Larry", score: 60 }, // 1
-    {id: "4455555", name: "Joseph", score: 50 }, // 2
+    {id: "1122222", name: "John", score: 90 },
+    {id: "2223333", name: "Larry", score: 60 },
+    {id: "4455555", name: "Joseph", score: 50 },
     {id: "5526666", name: "Karla", score: 80}
 ];
 
-document.write("<h3>JSON</h3>");
-document.write("<pre class='alert alert-secondary'>"); // 1) Bootstrap class
-document.write(JSON.stringify(students, undefined, 2));
-document.write("</pre>");
-document.write("<br/>");
-var names = [];
 
-students.forEach(student => {
-    names.push(student.name);
-});
 
-document.writeln("Students are:" + names);
 
-document.write("<pre>");
-document.writeln(`Average: ${calculateAverage()}`);
-document.write("</pre>");
+function loadData(){
+    
+    let JSON_data = document.getElementById("JSON_data");
+    JSON_data.innerText = JSON.stringify(students, undefined, 2);
 
-function calculateAverage(){
+    var names = [];
+        students.forEach(student => {
+        names.push(student.name);
+        });
 
-    var average = 0;
-    students.forEach(student => {
-        average = average + student.score;
-    });
-    average = average / students.length;
+    let initalSummary = document.getElementById("initalSummary")
+    var par1 = document.createElement("p");
+    par1.innerText = "Students are: " +names;
+    initalSummary.appendChild(par1);
+    var par2 = document.createElement("p");
+    par2.innerText = "Average: " + calculateAverage();
+    initalSummary.appendChild(par2);
+    
+    loadStudents()
 
-    return average;
 }
 
-function loadDataGrid() {
+/*function refreshTable(){
+    loadStudents()
+    let children = textTable.childNodes;
 
-    var i = 0;
-    let dataList = document.getElementById("dataList");
+    children.forEach(child => {
+        child.classList.remove("failed");
+    });
     
-    while (i < students.length)
-    {
-        var listItem = document.createElement("section");
-        listItem.classList.add("row");
-        
-        var id = document.createElement("div");
-        id.classList.add("col-sm");
-        id.innerText =  students[i].id;
+}*/
 
-        var name = document.createElement("div");
-        name.classList.add("col-sm");
-        name.innerText =  students[i].name;
-
-        var score = document.createElement("div");
-        score.classList.add("col-sm");
-        score.innerText =  students[i].score;
-
-        console.log(students[i]);
-
-        dataList.appendChild(listItem);
-
-        listItem.appendChild(id);
-        listItem.appendChild(name);
-        listItem.appendChild(score);
-
-        i = i + 1; // Alternatively, use i++;
-
-        // Other ways:
-        // i += 2;
-        // i += 3;
+function refreshTable(){
+    let textTable = document.getElementById("textTable");
+    
+    while (textTable.hasChildNodes()) {
+        textTable.removeChild(textTable.lastChild);
     }
+    loadStudents()
+}
+
+function loadStudents() {
+    var i = 0;
+    let textTable = document.getElementById("textTable");
+
+    while (i < students.length){
+        // first I create the section class row
+        var rowItem = document.createElement("section"); // create section
+        rowItem.classList.add("row"); //class row
+        textTable.appendChild(rowItem); // append the row to the container
+        
+        // now I create the 3 columns
+        var colItem1 = document.createElement("section"); // create section
+        colItem1.classList.add("col-sm"); // class col-sd
+        colItem1.innerText = students[i].id; // add text to the column, the student's id
+        rowItem.appendChild(colItem1); // append the column to the row
+
+        
+        var colItem2 = document.createElement("section"); // create section
+        colItem2.classList.add("col-sm"); // class col-sd
+        colItem2.innerText = students[i].name; // add text to the column, the student's name
+        rowItem.appendChild(colItem2); // append the column to the row
+
+        var colItem3 = document.createElement("section"); // create section
+        colItem3.classList.add("col-sm"); // class col-sd
+
+        let pass = document.getElementById("passingScore").value;
+        if (students[i].score < pass) {
+            colItem3.classList.add("failed");
+            colItem3.innerText = students[i].score;
+            rowItem.appendChild(colItem3);
+        }
+        else {
+            colItem3.innerText = students[i].score;
+            rowItem.appendChild(colItem3);
+        }
+        i = i + 1; 
+    }
+    
+}
+
+function calculateAverage(){
+    var average = 0;
+    students.forEach(student => {
+        average = average + student.score; });
+    average = average / students.length;
+    return average;
 }
 
 function displayAverage()
 {
-    var resultSection = document.getElementById("resultSection");
-    var paragraph = document.createElement("p");
-    paragraph.classList.add("badge"); // 2) Bootstrap classes
-    paragraph.classList.add("badge-info");
-
+    var paragraph = document.getElementById("paragraph");
+    //var paragraph = document.createElement("p");
+    paragraph.classList.add("badge");
+    paragraph.classList.add("badge-info")    
     paragraph.innerText = "Average: " + calculateAverage();
-
     resultSection.appendChild(paragraph);
 }
 
@@ -90,25 +115,3 @@ function myReplacer(name, val) {
         return val; // return as is
     }
 };
-
-// Old-way of loading data (ol). No longer used
-function loadData(){
-
-    var i = 0;
-    let dataList = document.getElementById("dataList");
-    
-    while (i < students.length)
-    {
-        var listItem = document.createElement("li");
-        
-        console.log(students[i]);
-        listItem.innerText = students[i].name;
-
-        dataList.appendChild(listItem);
-        i = i + 1; // Alternatively, use i++;
-
-        // Other ways:
-        // i += 2;
-        // i += 3;
-    }
-}
